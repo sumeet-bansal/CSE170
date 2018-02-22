691d0bf2875c9a349e1c6ceac3d10c56378a580c
Sumeet Bansal <sumeetbansal@gmail.com>
2018-02-21 23:22:46 -0800

    made SQL queries return in random order for non-deterministic meal plans

--- routes/results.js
+++ routes/results.js
@@ -16,8 +16,9 @@ exports.generatePlan = function(req, res) {
 			var max = prefs.max;
 			var price = prefs.price;
 			var calories = ' AND calories > ' + min/num + ' AND calories < ' + max/num;
-			var sorted = ' ORDER BY calories DESC LIMIT ' + num;
-			sql = 'SELECT * FROM items WHERE' + dietary + location + calories + sorted;
+			var random = ' AND _ROWID_ >= (abs(random()) % (SELECT max(_ROWID_) FROM items))';
+			var limit = ' LIMIT ' + num;
+			sql = 'SELECT * FROM items WHERE' + dietary + location + calories + random + limit;
 			console.log(sql);
 			var meals = [], sum = 0, budget = 0;
 			db.each(sql, function(err, row) {
