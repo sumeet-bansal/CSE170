30ec14482a0db753dd462c9c7dda65b9c2431671
Sumeet Bansal <sumeetbansal@gmail.com>
2018-02-22 19:38:43 -0800

    fixed DB locations

---
+++ db-processing.sql
@@ -0,0 +1,6 @@
+UPDATE items SET location = "Sixth" WHERE location = "Foodworx";
+UPDATE items SET location = "Marshall" WHERE location = "OceanView";
+UPDATE items SET location = "Revelle" WHERE location = "64 Degrees";
+UPDATE items SET location = "Warren" WHERE location = "Canyon Vista";
+UPDATE items SET location = "Muir" WHERE location = "Pines";
+UPDATE items SET location = "ERC" WHERE location LIKE "%Ventanas%";
--- routes/script.sql
+++
@@ -1,6 +0,0 @@
-UPDATE items SET calories = ABS(RANDOM() % 1200), location = "Sixth" WHERE location = "Foodworx";
-UPDATE items SET calories = ABS(RANDOM() % 1200), location = "Marshall" WHERE location = "OceanView";
-UPDATE items SET calories = ABS(RANDOM() % 1200), location = "Revelle" WHERE location = "64 Degrees";
-UPDATE items SET calories = ABS(RANDOM() % 1200), location = "Warren" WHERE location = "Canyon Vista";
-UPDATE items SET calories = ABS(RANDOM() % 1200), location = "Muir" WHERE location = "Pines";
-UPDATE items SET calories = ABS(RANDOM() % 1200), location = "ERC" WHERE location LIKE "%Ventanas%";
