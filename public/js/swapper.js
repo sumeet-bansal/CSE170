3cd7c2601ddc19ea9985db32dd24b8193f74bce7
Sumeet Bansal <sumeetbansal@gmail.com>
2018-02-27 17:40:54 -0800

    more clearly separated calorie and price info on item cards

--- public/js/swapper.js
+++ public/js/swapper.js
@@ -15,13 +15,13 @@ function swap(e) {
 	var summary = sumel.text();
 
 	var el = $(this).closest('.card-body');
-	var item = el.find('.meal-details').html();
+	var item = el.find('.meal-details');
 
 	var calsum = parseInt(summary.substring(summary.indexOf('with ') + 'with '.length, summary.indexOf(' cal')));
 	var pricesum = parseFloat(summary.substring(summary.indexOf('$') + 1));
 
-	var calories = parseInt(item.substring(0, item.indexOf(' ')));
-	var price = parseFloat(item.substring(item.indexOf('$') + 1));
+	var calories = parseInt(item.find('#item-cal').text().substring(0, item.find('#item-cal').text().indexOf(' ')));
+	var price = parseFloat(item.find('#item-dd').text().substring(1));
 
 	calsum -= calories;
 	pricesum -= price;
@@ -42,12 +42,12 @@ function swap(e) {
 		el.html(result.html());
 		el.find('.swap-link').click(swap);
 
-		item = result.find('.meal-details').html();
+		item = result.find('.meal-details');
 		if (item == undefined) {
 			alert("No similar item could be found.");
 		} else {
-			calsum += parseInt(item.substring(0, item.indexOf(' ')));
-			pricesum += parseFloat(item.substring(item.indexOf('$') + 1));
+			calsum += parseInt(item.find('#item-cal').text().substring(0, item.find('#item-cal').text().indexOf(' ')));
+			pricesum += parseFloat(item.find('#item-dd').text().substring(1));
 			summary = "<h4>Here's your custom meal plan with " + calsum +" calories at $" + pricesum.toFixed(2) +".</h4>";
 			sumel.html(summary);
 		}
